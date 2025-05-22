bff7ed0ba224814bdb93e7e131d21a85d1735260
Patrick Sardinha <patricksardinha.pro@gmail.com>
2025-05-22 23:55:32 +0200

    test setup

---
+++ TailwindWPF.Test/TwClassTests.cs
@@ -0,0 +1,21 @@
+using System.Windows;
+using System.Windows.Controls;
+using TailwindWPF.Styling;
+using Xunit;
+using Xunit.Sdk;
+
+namespace TailwindWPF.Test
+{
+    public class TwClassTest
+    {
+
+        
+            [WpfFact]
+            public void CanRunStaTest()
+            {
+                Assert.True(true);
+            }
+        
+    
+    }
+}
