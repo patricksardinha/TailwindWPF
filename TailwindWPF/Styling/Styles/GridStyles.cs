545af12e778cebd5d451390a1def19f00391a353
Patrick Sardinha <patricksardinha.pro@gmail.com>
2025-05-29 22:56:56 +0200

    Setup for grid but need def grid mapping for WPF grid

---
+++ TailwindWPF/Styling/Styles/GridStyles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TailwindWPF.Styling.Styles
+{
+    public class GridStyles : BaseStyleProvider
+    {
+
+        public override Dictionary<string, Action<DependencyObject>> GetStyles() => new()
+        {
+        };
+    }
+}
